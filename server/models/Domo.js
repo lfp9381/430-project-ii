408125a57b14771d74a08d53dbe4e59cb73cc20d
lfp9381 <lfp9381@rit.edu>
2025-11-29 21:47:27 -0500

    moved over domo files

---
+++ server/models/Domo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+
+const setName = (name) => _.escape(name).trim();
+
+const DomoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  food: {
+    type: String,
+    required: true,
+    trim: true,
+    // set: setName,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+DomoSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  age: doc.age,
+  food: doc.food,
+});
+
+const DomoModel = mongoose.model('Domo', DomoSchema);
+module.exports = DomoModel;
